e92669bd849d52787502f486409072f589a0315b
HamsterJesus <2104302@rgu.ac.uk>
2023-03-06 15:04:51 +0000

    yup

---
+++ lab6/main/java/uk/ac/rgu/cm2116/Parser.java
@@ -0,0 +1,8 @@
+package uk.ac.rgu.cm2116;
+
+public class Parser {
+    
+    public boolean parse(String input){
+        return false;
+    }
+}
---
+++ lab6/main/java/uk/ac/rgu/cm2116/Stack.java
@@ -0,0 +1,29 @@
+package uk.ac.rgu.cm2116;
+
+public class Stack<T> implements AbstractStack<T>{
+
+    @Override
+    public boolean push(T element) throws StackOverflowException {
+        return false;
+    }
+
+    @Override
+    public T pop() throws StackUnderFlowException {
+        return null;
+    }
+
+    @Override
+    public T peek() throws StackUnderFlowException {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return (this.size() == 0);
+    }
+}
