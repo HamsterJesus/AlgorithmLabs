72ee0c40e40112335ac6dd0ccd3a15696dbbe50c
HamsterJesus <2104302@rgu.ac.uk>
2023-03-06 15:44:41 +0000

    fgd

--- lab6/main/java/uk/ac/rgu/cm2116/Queue.java
+++ lab6/main/java/uk/ac/rgu/cm2116/Queue.java
@@ -3,25 +3,59 @@ package uk.ac.rgu.cm2116;
 import java.util.NoSuchElementException;
 
 public class Queue<T> extends SoCQueue<T> {
+    private Object[] queue;
+    private int front;
+    private int rear;
+    private int size;
+
+    public Queue(){
+        this.queue = new Object[5];
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean add(T e) throws IllegalStateException {
-        return false;
+        int index = this.rear % this.queue.length;
+        if(this.front == index && this.size != 0){
+            throw new IllegalStateException("waahhh");
+        }
+
+        this.queue[index] = e;
+        this.rear = this.rear+1;
+        this.size = this.size+1;
+        return true;
     }
 
     @Override
     public boolean offer(T e) {
-        return false;
+        int index = this.rear % this.queue.length;
+        if(this.front == index && this.size != 0){
+            return false;
+        }
+
+        this.queue[index] = e;
+        this.rear = this.rear+1;
+        this.size = this.size+1;
+        return true;
     }
 
     @Override
     public T remove() {
-        return null;
+        if(this.size == 0){
+            return null;
+        }
+
+        T element = (T) this.queue[this.front % this.queue.length];
+        this.front = this.front + 1;
+        this.size = this.size - 1;
+        return element;
     }
 
     @Override
--- lab6/main/java/uk/ac/rgu/cm2116/Stack.java
+++ lab6/main/java/uk/ac/rgu/cm2116/Stack.java
@@ -1,25 +1,50 @@
 package uk.ac.rgu.cm2116;
 
 public class Stack<T> implements AbstractStack<T>{
+    private Object[] stack;
+    private int top;
+
+    public Stack(){
+        this.stack = new Object[10];
+        this.top = -1;
+    }
 
     @Override
     public boolean push(T element) throws StackOverflowException {
-        return false;
+        if(this.top == (this.stack.length - 1)){
+            throw new StackOverflowException("Whoopsie");
+        }
+
+        this.top = this.top -1;
+        this.stack[this.top] = element;
+        return true;
     }
 
     @Override
     public T pop() throws StackUnderFlowException {
-        return null;
+        if(this.top < 0){
+            throw new StackUnderFlowException("OOchie");
+        }
+
+        T element = (T) this.stack[this.top];
+        this.stack[this.top] = null;
+
+        this.top = this.top -1;
+        return element;
     }
 
     @Override
     public T peek() throws StackUnderFlowException {
-        return null;
+        if(this.top < 0){
+            throw new StackUnderFlowException("uh oh spaghettio");
+        }
+
+        return (T) this.stack[this.top];
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.top + 1;
     }
 
     @Override
