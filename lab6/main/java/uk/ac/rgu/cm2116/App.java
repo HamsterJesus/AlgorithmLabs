9df378784cf96fd532c959de8b23eefd9b064516
HamsterJesus <2104302@rgu.ac.uk>
2023-03-13 15:22:15 +0000

    cahnag

--- lab6/main/java/uk/ac/rgu/cm2116/App.java
+++ lab6/main/java/uk/ac/rgu/cm2116/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        Person person = new Person;
+        
         System.out.println( "Hello World!" );
     }
 }
--- lab6/main/java/uk/ac/rgu/cm2116/Parser.java
+++ lab6/main/java/uk/ac/rgu/cm2116/Parser.java
@@ -3,6 +3,7 @@ package uk.ac.rgu.cm2116;
 public class Parser {
     
     public boolean parse(String input){
+        
         return false;
     }
 }
