b24932d8467b8b9883abd673bf41b9efd0c1c4df
HamsterJesus <n.murchie@rgu.ac.uk>
2023-03-27 00:56:13 +0100

    time efficency study

--- Lab2App.java
+++ Lab2App.java
@@ -8,11 +8,12 @@ public class Lab2App {
     static int count = 0;
 
     public static void main(String[] args) throws Exception {
-        testSortingAlgorithm(Lab2App::selectionSort, "selectionSort.csv");
-        testSortingAlgorithm(Lab2App::bubbleSort, "bubblesort.csv");
-        testSortingAlgorithm(Lab2App::bubbleSortEfficient, "bubblesortefficient.csv");
-        TestSortingForSearch(Lab2App::LinearSearch, "LinearSearch.csv");
-        testBinarySearch(Lab2App::BinarySearch, "BinarySearch.csv");
+        // testSortingAlgorithm(Lab2App::selectionSort, "selectionSort.csv");
+        // testSortingAlgorithm(Lab2App::bubbleSort, "bubblesort.csv");
+        // testSortingAlgorithm(Lab2App::bubbleSortEfficient, "bubblesortefficient.csv");
+        // TestSortingForSearch(Lab2App::LinearSearch, "LinearSearch.csv");
+        testAddToArray("AddToArray.csv");
+        // testLinearSearch("LinearSearch.csv");
 
         //testing
         //int tempArray[] = {1,2,3,4,5};
@@ -77,42 +78,65 @@ public class Lab2App {
         }
     }
 
-    static int LinearSearch(int[] input, int target){
+    static int LinearSearch(int input[], int target){
         count = 0;
         for(int i = 0; i<input.length-1; i++){
             count++;
             if(input[i] == target){
+                count++;
                 return i;
             }
         }
-
         return -1;
     }
 
-    //logarithmic search algorithmn (must use sorted array)
-    static int BinarySearch(int[] input, int target){
-        //define high low and mid
+    static int BinarySearch(int input[], int target){
+        int low = 0, high = input.length-1, mid = 0;
         count = 0;
-        int low = 0;
-        int high = input.length-1;
-        int mid = 0;
 
         while(low <= high){
-            mid = (int) Math.floor((low+high)/2);
-            //System.out.println(mid + "inside");
             count++;
-            if(input[mid] == target){ //if target is at the middle of the array
+            mid = (int) Math.floor((high+low)/2); //find mid point
+
+            if(input[mid] == target){ //if target found
+                count++;
                 return mid;
-            } else if(target < input[mid]){ //if target is on left side of sorted array
-                high = mid -1;
-            } else { //if target is on right side of sorted array
-                low = mid + 1;   
+            } else if(target < input[mid]){ //if target is on the left
+                count++;
+                high = mid -1; //shift left in array
+            } else { //if target is on the right
+                count++;
+                low = mid +1; //shift right in array
             }
         }
 
-        System.out.println(count);
 
-        return -1;
+        //return -1; //target not found
+        return mid; //adapted version
+    }
+
+    static int[] AddToArray(int[] input, int element){
+        count = 0;
+        int[] newArray = new int[input.length+1];
+        int position = BinarySearch(input, element);
+
+        if(element>input[position]){
+            position = position +1;
+        }
+
+        newArray[position] = element;
+
+        for(int i = 0; i<position -1; i++){
+            count++;
+            newArray[i] = input[i];
+        }
+
+        for(int i = position+1; i<newArray.length-1; i++){
+            count++;
+            newArray[i] = input[i-1];
+        }
+
+        return newArray;
     }
 
     static void testSortingAlgorithm(SortingAlgorithm sorter, String outputFilename) {
@@ -252,17 +276,101 @@ public class Lab2App {
             int[] averageArray = getAverageCaseArray(i);
             //int[] worstArray = getWorstCaseArray(i);
 
-            int tempBest = averageArray[(int) (Math.floor(averageArray.length-1)/2)]; //first item in array
+            int tempBest = averageArray[(int) Math.floor(start/2)]; //first item in array
             sorter.sort(averageArray, tempBest);
             best[i - start] = count;
 
-            int tempAvg = averageArray[(int) Math.floor(Math.random()*i)]; //random item in array
+            int randomPosition = (int) (Math.floor(Math.random()*start));
+            int tempAvg = averageArray[randomPosition]; //random item in array
             sorter.sort(averageArray, tempAvg);
             average[i - start] = count;
 
-            int tempWorst = averageArray[averageArray.length-1]; //last item in array
+            int tempWorst = averageArray[start]; //last item in array
             sorter.sort(averageArray, tempWorst);
             worst[i - start] = count;
+        }
+
+        StringBuilder output = new StringBuilder();
+        output.append("n,ops-best,ops-average,ops-worst\n");
+
+        for (int i = 0; i < best.length; i++) {
+            int n = i + start;
+            output.append(n)
+                  .append(",")
+                  .append(best[i])
+                  .append(",")
+                  .append(average[i])
+                  .append(",")
+                  .append(worst[i])
+                  .append("\n");
+        }
+        writeToCSV(outputFilename, output.toString());
+    }
+
+    static void testLinearSearch(String outputFilename){
+        //file sizes for each
+        int start =5, end = 100;
+
+        StringBuilder output = new StringBuilder();
+
+        output.append("n,ops-best,ops-average,ops-worst\n");
+
+        for(int i = start; i<end; i++){
+            int[] bestArray = getBestCaseArray(i);
+            int[] averageArray = getAverageCaseArray(i);
+            int[] worstArray = getWorstCaseArray(i);
+
+            //best case target is the first value in the array
+            int bestTarget = bestArray[0];
+            int best = LinearSearch(bestArray, bestTarget);
+
+            //average case target position is random
+            int randomPosition = (int) (Math.floor(Math.random()*start));
+            int averageTarget = averageArray[randomPosition];
+            int average = LinearSearch(averageArray, averageTarget);
+            //best[i - start] = count;
+
+            //worst case target position is at the end
+            int worstTarget = worstArray[start];
+            int worst = LinearSearch(worstArray, worstTarget);
+            //best[i - start] = count;
+
+            int n = i + start;
+            output.append(n)
+                  .append(",")
+                  .append(best)
+                  .append(",")
+                  .append(average)
+                  .append(",")
+                  .append(worst)
+                  .append("\n");
+        }
+
+        writeToCSV(outputFilename, output.toString());
+    }
+
+    static void testAddToArray(String outputFilename) {
+
+        int start = 5, end = 100;
+
+        int[] best = new int[end - start];
+        int[] average = new int[end - start];
+        int[] worst = new int[end - start];
+
+        for (int i = 5; i < 100; i++) {
+            int[] averageArray = getAverageCaseArray(i);
+            bubbleSortEfficient(averageArray);
+
+            int tempBest = averageArray[start]+1; //first item in array
+            //tempBest++;
+            best = AddToArray(averageArray, tempBest);
+
+            int randomPosition = (int) (Math.floor(Math.random()*start));
+            int tempAvg = averageArray[randomPosition]+1; //random item in array
+            average = AddToArray(averageArray, tempAvg);
+
+            int tempWorst = averageArray[0]-1; //last item in array
+            worst = AddToArray(averageArray, tempWorst);
         }
 
         StringBuilder output = new StringBuilder();
